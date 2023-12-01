6d65acdb9296cec4dd51736f49c6bf8f8614721a
AmyE123 <amy.elliott2002@yahoo.co.uk>
2023-12-01 00:14:08 +0000

    Added consistent documentations throughout Constants.cs

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,37 +1,56 @@
 namespace CT6GAMAI
 {
+    /// <summary>
+    /// Contains constants and enums used throughout the game.
+    /// </summary>
     public static class Constants
     {
         #region Nodes
         /// <summary>
-        /// The enumerations representing the different visual states of a node
+        /// Enumerations representing the different visual states of a node.
         /// </summary>
         public enum NodeVisualState { Default, HoveredBlue, HoveredRed, HoveredGreen, SelectedBlue, SelectedRed, SelectedGreen, AllEnemyRange, SingularEnemyRange, PointOfInterest }
 
         /// <summary>
-        /// The enumerations representing the different color states of a node
+        /// Enumerations representing the different color states of a node.
         /// </summary>
         public enum NodeVisualColorState { Blue, Red, Green }
 
         /// <summary>
-        /// The enumerations representing the different enemy color states of a node
+        /// Enumerations representing the different enemy color states of a node.
         /// </summary>
         public enum NodeVisualEnemyColorState { SingularEnemy, AllEnemy }
 
         /// <summary>
-        /// The enumerations representing the different states of a selector
+        /// Enumerations representing the different states of a selector.
         /// </summary>
         public enum NodeSelectorState { NoSelection, DefaultSelected, PlayerSelected, EnemySelected }
 
+        /// <summary>
+        /// Enumerations representing different directions.
+        /// </summary>
         public enum Direction { North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest }
+
+        /// <summary>
+        /// Enumerations representing different terrain types.
+        /// </summary>
         public enum Terrain { Default, Forest, River, Fort }
 
+        /// <summary>
+        /// Constant string for the tag string used on nodes.
+        /// </summary>
         public const string NODE_TAG_REFERENCE = "Node";
         #endregion //Nodes
 
         #region Units
+        /// <summary>
+        /// Enumerations representing different unit classes.
+        /// </summary>
         public enum Class { Knight, Mercenary, Archer };
 
+        /// <summary>
+        /// Constant string for the tag string used on units.
+        /// </summary>
         public const string UNIT_TAG_REFERENCE = "Unit";
         #endregion //Units                           
     }
