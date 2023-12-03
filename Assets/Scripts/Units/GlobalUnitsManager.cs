6ad14c40712fa26c8f3efdd592ae5cab20aa5eb0
AmyE123 <amy.elliott2002@yahoo.co.uk>
2023-12-03 22:53:04 +0000

    Added documentation to GlobalUnitsManager.cs and updated variable order

--- Assets/Scripts/Units/GlobalUnitsManager.cs
+++ Assets/Scripts/Units/GlobalUnitsManager.cs
@@ -4,15 +4,25 @@ namespace CT6GAMAI
     using System.Linq;
     using UnityEngine;
 
+    /// <summary>
+    /// Manages all units in the game, handling their initialization and state.
+    /// </summary>
     public class GlobalUnitsManager : MonoBehaviour
     {
         [SerializeField] private List<UnitManager> _allUnits;
         [SerializeField] private List<UnitManager> _activeUnits;
 
-        public List<UnitManager> AllUnits => _allUnits;
-        public List<UnitManager> ActiveUnits => _activeUnits;
-
         private bool _unitsInitalized = false;
+
+        /// <summary>
+        /// Gets the list of all units in the game.
+        /// </summary>
+        public List<UnitManager> AllUnits => _allUnits;
+
+        /// <summary>
+        /// Gets the list of active units in the game.
+        /// </summary>
+        public List<UnitManager> ActiveUnits => _activeUnits;     
 
         private void Update()
         {           
