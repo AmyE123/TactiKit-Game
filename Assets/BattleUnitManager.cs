27d932cea0429b64d3aeeea81997a4900a2f4292
AmyE123 <amy.elliott2002@yahoo.co.uk>
2023-12-23 18:26:35 +0000

    BattleUnitManager, Headers, Docs & Formatting

--- Assets/BattleUnitManager.cs
+++ Assets/BattleUnitManager.cs
@@ -3,21 +3,52 @@ namespace CT6GAMAI
     using UnityEngine;
     using static CT6GAMAI.Constants;
 
+    /// <summary>
+    /// Manages the battle properties and behaviors of a unit in the game.
+    /// </summary>
     public class BattleUnitManager : MonoBehaviour
     {
+        [Header("Unit Identification")]
         [SerializeField] private Side _unitSide;
-        [SerializeField] private Animator _animator;
         [SerializeField] private UnitManager _unitManagerRef;
-        [SerializeField] private bool _canUnitAttackAgain;
-        [SerializeField] private bool _unitCompleteAttacks;
         [SerializeField] private UnitStatsManager _unitStatsManagerRef;
 
+        [Header("Unit Animation")]
+        [SerializeField] private Animator _animator;
+
+        [Header("Unit Status")]
+        [SerializeField] private bool _canUnitAttackAgain;
+        [SerializeField] private bool _unitCompleteAttacks;
+
+        /// <summary>
+        /// Gets the side which the unit is on.
+        /// </summary>
         public Side UnitSide => _unitSide;
+
+        /// <summary>
+        /// Gets the animator commonent attached to the battle unit.
+        /// </summary>
         public Animator Animator => _animator;
-        public bool CanUnitAttackAgain => _canUnitAttackAgain;
-        public bool UnitCompleteAttacks => _unitCompleteAttacks;
+
+        /// <summary>
+        /// Gets the UnitManager reference for the unit.
+        /// </summary>
         public UnitManager UnitManagerRef => _unitManagerRef;
+
+        /// <summary>
+        /// Gets the UnitStatsManager reference for the unit.
+        /// </summary>
         public UnitStatsManager UnitStatsManager => _unitStatsManagerRef;
+
+        /// <summary>
+        /// Whether the unit can attack again.
+        /// </summary>
+        public bool CanUnitAttackAgain => _canUnitAttackAgain;
+
+        /// <summary>
+        /// Whether the uni has completed all of its attacks.
+        /// </summary>
+        public bool UnitCompleteAttacks => _unitCompleteAttacks;
 
         public void SetUnitCompleteAttacks(bool value)
         {
