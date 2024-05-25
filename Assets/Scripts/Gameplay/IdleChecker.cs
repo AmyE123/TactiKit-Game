b1021b65b1d288a979d8162309025649ecff6839
AmyE123 <amy.elliott2002@yahoo.co.uk>
2024-05-25 09:13:26 +0100

    Moved scripts to right folder and added restart button & idle checker to main game

---
+++ Assets/Scripts/Gameplay/IdleChecker.cs
@@ -0,0 +1,44 @@
+namespace CT6GAMAI
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class IdleChecker : MonoBehaviour
+    {
+        public float idleTime = 600f;
+        private float idleTimer = 0f;
+
+        void Update()
+        {
+            // Check for any input
+            if (Input.anyKeyDown || Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+            {
+                // Reset the timer on any input
+                idleTimer = 0f;
+            }
+            else
+            {
+                // Increment the timer if no input is detected
+                idleTimer += Time.deltaTime;
+            }
+
+            // Check if the idle timer has exceeded the idle time
+            if (idleTimer >= idleTime)
+            {
+                // Load the menu scene if the idle time is reached
+                LoadMenuScene();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Minus))
+            {
+                LoadMenuScene();
+            }
+        }
+
+        private void LoadMenuScene()
+        {
+            // Replace "MenuScene" with the actual name of your menu scene
+            SceneManager.LoadScene("TitleScreen");
+        }
+    }
+}
